5de4538521dabcdd7943fa4290806930eff43be2
Marius-Adam <marius.c.adam@gmail.com>
2020-05-31 14:10:23 +0100

    Updates

---
+++ precache-manifest.4d81ebbdb381e27061a117a617c4f7ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1720483ee0010589481e76ed933e262",
+    "url": "/react-quizz-app/index.html"
+  },
+  {
+    "revision": "7ab8bbd663283b39e150",
+    "url": "/react-quizz-app/static/css/main.bc290086.chunk.css"
+  },
+  {
+    "revision": "3999b93b4d073a41da76",
+    "url": "/react-quizz-app/static/js/2.81564809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quizz-app/static/js/2.81564809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab8bbd663283b39e150",
+    "url": "/react-quizz-app/static/js/main.19a9826e.chunk.js"
+  },
+  {
+    "revision": "7e2204b14d05115abe74",
+    "url": "/react-quizz-app/static/js/runtime-main.ae310e05.js"
+  }
+]);
